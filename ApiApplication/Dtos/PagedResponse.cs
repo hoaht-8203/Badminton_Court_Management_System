ab0c0cbc49379fbed5ba18224d3c0566a887d82d
khanhph <phamhuykhanh1111@gmail.com>
2025-09-26 23:57:10 +0700

    Demo pagination customer

---
+++ ApiApplication/Dtos/PagedResponse.cs
@@ -0,0 +1,10 @@
+namespace ApiApplication.Dtos;
+
+public class PagedResponse<T>
+{
+    public List<T> Items { get; set; } = new List<T>();
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public long TotalItems { get; set; }
+    public int TotalPages { get; set; }
+}
---
+++ ApiApplication/Migrations/20250926151758_fixbase.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixbase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AvatarUrl",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("ba8008d3-6f25-4ca3-be62-21c2af0e4f97"),
+                columns: new[] { "AvatarUrl", "PasswordHash" },
+                values: new object[] { null, "AQAAAAIAAYagAAAAEPe03q/HkHdadu1LfusfkS9J2m1iIgFq2aWIM+5hXxK5X74ox6bK7U4lGGpHga6e8Q==" });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 9, 26, 15, 17, 56, 829, DateTimeKind.Utc).AddTicks(285), new DateTime(2025, 9, 26, 15, 17, 56, 829, DateTimeKind.Utc).AddTicks(287) });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 9, 26, 15, 17, 56, 829, DateTimeKind.Utc).AddTicks(291), new DateTime(2025, 9, 26, 15, 17, 56, 829, DateTimeKind.Utc).AddTicks(292) });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 9, 26, 15, 17, 56, 829, DateTimeKind.Utc).AddTicks(294), new DateTime(2025, 9, 26, 15, 17, 56, 829, DateTimeKind.Utc).AddTicks(294) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvatarUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("ba8008d3-6f25-4ca3-be62-21c2af0e4f97"),
+                column: "PasswordHash",
+                value: "AQAAAAIAAYagAAAAEF/WWUNVCI9Ogb2n7zCczp6wC0s0dyU1P+Q0v+bL3B6mTaEzcZEm8Arn5btaos8b7w==");
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 9, 26, 11, 44, 31, 251, DateTimeKind.Utc).AddTicks(2600), new DateTime(2025, 9, 26, 11, 44, 31, 251, DateTimeKind.Utc).AddTicks(2600) });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 9, 26, 11, 44, 31, 251, DateTimeKind.Utc).AddTicks(2610), new DateTime(2025, 9, 26, 11, 44, 31, 251, DateTimeKind.Utc).AddTicks(2610) });
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2025, 9, 26, 11, 44, 31, 251, DateTimeKind.Utc).AddTicks(2610), new DateTime(2025, 9, 26, 11, 44, 31, 251, DateTimeKind.Utc).AddTicks(2610) });
+        }
+    }
+}
